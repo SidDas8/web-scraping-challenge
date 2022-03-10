7240644d254bd2d14dbb1ac0a196a98e3b1ebf2c
SidDas8 <siddharthdas168@gmail.com>
2022-03-10 01:24:05 -0800

    WOrking on scrape.

--- Missions_to_Mars/app/app.py
+++ Missions_to_Mars/app/app.py
@@ -6,9 +6,8 @@ import scrape_mars
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
